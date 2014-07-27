d99fa09752b3971d9a56fad693309d55a7e3da50
StevenGao89 <XiaotianGao@Xiaotians-MacBook-Pro.local>
2014-07-27 17:26:51 -0400

    edited
    Hopefully it will work.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are the functions calculate the inverse of a given matrix. However,
+## the calculation of a matrix can be expensive if the dimension of the matrix
+## is large, so these functions could cache the already calculated results to
+## skip redundant calculations.
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix creates a special "vector", which is 
+##really a list containing a function to
+
+##set the value of the matrix
+##get the value of the matrix
+##set the value of the inverse of the matrix
+##get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) i <<- solve
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the mean of the special "vector" created 
+##with the above function. However, it first checks to see if the inverse has 
+##already been calculated. If so, it gets the inverse from the cache and skips the 
+##computation. Otherwise, it calculates the inverse of the data and sets the value 
+##of the inverese in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
+        
+        
 }
